cfd3de7793c5d377ff125b76fdcc0118f1fc8b4a
yindiana <yndiana.contact@gmail.com>
2024-04-17 20:52:58 +0200

    refactored en cours

--- scripts/scripts__switch-article.js
+++ scripts/scripts__switch-article.js
@@ -1,6 +1,28 @@
-// Écouter l'événement de défilement de la page
-window.addEventListener('scroll', () => {
-// Vérifier si la propriété display de l'élément avec l'ID home-page est à flex
-    const homePage = document.getElementById('home-page');
-    console.log(homePage)
-});
+// déclaration des variables globales + events pour update des articles
+document.getElementById('nav__logo').addEventListener('click', updateDisplayFlex4Homepage); /*page d'acceuil*/
+document.getElementById('nav__menu-home').addEventListener('click', updateDisplayFlex4Homepage); /*page d'acceuil*/
+document.getElementById('nav__menu-about').addEventListener('click', updateDisplayFlex4Portfolio); /*page portfolios*/
+document.getElementById('nav__menu-contacts').addEventListener('click', updateDisplayFlex4Contacts); /*page de contacts*/
+const listOfIdOfArticle = ['home-page','carousel-container','contacts'];
+
+// permet le switch entre les articles
+function updateDisplayFlex4Homepage() {
+    document.getElementById(listOfIdOfArticle[0]).style.display = 'flex';
+    // masque les deux autres articles
+    document.getElementById(listOfIdOfArticle[1]).style.display = 'none';
+    document.getElementById(listOfIdOfArticle[2]).style.display = 'none';
+}
+
+function updateDisplayFlex4Portfolio() {
+    document.getElementById(listOfIdOfArticle[1]).style.display = 'flex';
+    // masque les deux autres articles
+    document.getElementById(listOfIdOfArticle[0]).style.display = 'none';
+    document.getElementById(listOfIdOfArticle[2]).style.display = 'none';
+}
+
+function updateDisplayFlex4Contacts() {
+    document.getElementById(listOfIdOfArticle[2]).style.display = 'flex';
+    // masque les deux autres articles
+    document.getElementById(listOfIdOfArticle[0]).style.display = 'none';
+    document.getElementById(listOfIdOfArticle[1]).style.display = 'none';
+}
