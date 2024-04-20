1ebc5bd0e544c0cc86b1787f53e4fbad7427cbe8
yindiana <yndiana.contact@gmail.com>
2024-04-20 18:36:19 +0200

    refactored version desktop en cours

--- scripts/scripts_switch-article.js
+++
@@ -1,6 +0,0 @@
-// Écouter l'événement de défilement de la page
-window.addEventListener('scroll', () => {
-// Vérifier si la propriété display de l'élément avec l'ID home-page est à flex
-    const homePage = document.getElementById('home-page');
-    console.log(homePage)
-});
